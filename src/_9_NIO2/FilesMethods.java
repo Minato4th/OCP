e11da5fbd54e7e177c20afb52e5db3f4d42c56ba
Minato4th <borisko.aleksey@gmail.com>
2019-08-09 17:23:34 +0300

    Update 6

--- src/_9_NIO2/FilesMethods.java
+++ src/_9_NIO2/FilesMethods.java
@@ -8,7 +8,7 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
-public class FilesMetods {
+public class FilesMethods {
 
     public static void main(String[] args) {
         exists();
